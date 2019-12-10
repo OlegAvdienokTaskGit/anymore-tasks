5411aaa568eb985d4984b65d15d0eff9778215b3
OlegAvdienokTaskGit <58690306+OlegAvdienokTaskGit@users.noreply.github.com>
2019-12-10 14:19:27 +0200

    Add files via upload

---
+++ 1_to_one.py
@@ -0,0 +1,6 @@
+"""
+Дана строка. Замените в этой строке все цифры 1 на слово one. Циклы не использовать.
+"""
+
+string = '1 man 1 girl 1 dog 1 cat 1 million dollors'
+print(string.replace("1", "one"))
